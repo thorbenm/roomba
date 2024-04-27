097f55a91b8fe82d69c65191a1429c957849e502
Thorben Muhleder <thorbenm@me.com>
2024-04-27 16:53:06 +0200

    first commit

---
+++ roomba.py
@@ -0,0 +1,61 @@
+from time import time
+from datetime import datetime
+import subprocess
+import os
+from login_data import IRBT_LOGIN, IRBT_PASSWORD
+import json
+
+
+def login():
+    os.environ['IRBT_LOGIN'] = IRBT_LOGIN
+    os.environ['IRBT_PASSWORD'] = IRBT_PASSWORD
+
+
+login()
+
+
+def force_start():
+    subprocess.run("irbt-cli.py -c start".split(), capture_output=True, text=True)
+
+
+def stop():
+    subprocess.run("irbt-cli.py -c stop".split(), capture_output=True, text=True)
+    subprocess.run("irbt-cli.py -c dock".split(), capture_output=True, text=True)
+
+
+def start():
+    if 7 <= datetime.now().hour < 20:
+        with open("/home/pi/Programming/roomba/disabled_until", "r") as f:
+            disabled_until = int(f.read())
+            if disabled_until < int(time()):
+                if get_cleaning_time_today() < 45:
+                    force_start()
+
+
+def get_cleaning_time_today():
+    last_midnight = datetime.combine(datetime.now().date(), datetime.min.time())
+    return get_cleaning_time_since(last_midnight)
+
+
+def get_cleaning_time_since(since):
+    # in minutes
+    output = subprocess.check_output('irbt-cli.py -M', shell=True, text=True)
+    data = json.loads(output)
+    timestamp_threshold = since.timestamp()
+    total_minutes = sum(item['durationM'] for item in data if timestamp_threshold <= item['timestamp'] )
+    return total_minutes
+
+
+def disable_for(time_string):
+    unit = time_string[-1]
+    value = time_string[:-1]
+    if unit == "s":
+        t = int(value)
+    if unit == "m":
+        t = int(value) * 60
+    if unit == "h":
+        t = int(value) * 60 * 60
+    if unit == "d":
+        t = int(value) * 60 * 60 * 24
+    with open("/home/pi/Programming/roomba/disabled_until", "w") as f:
+        f.write(str(int(time()) + t))
